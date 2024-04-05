a086c44957cbd81c214bb2ecd97e9c511da8e96c
SerapioUnlucky <sjerez957@gmail.com>
2024-04-05 14:22:57 -0300

    Se agrego crossOrigin

--- src/main/java/com/bolsasdete/springboot/apirestspringboot/Controllers/CustomerController.java
+++ src/main/java/com/bolsasdete/springboot/apirestspringboot/Controllers/CustomerController.java
@@ -19,6 +19,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +38,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RequestMapping("/api")
 public class CustomerController {
 
